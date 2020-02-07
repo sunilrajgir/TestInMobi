afe3180b2922ef4c15fc031575b3618f55d4bb72
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-02-07 17:33:20 +0530

    added collection view and search bar

--- InMobiTest/InMobiTest/TestViewController.swift
+++ InMobiTest/InMobiTest/TestViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class TestViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView!
+    
+    @IBOutlet var searcBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        searcBar.delegate = self
 
     }
 
 }
+
+extension TestViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+        
+}
+
+extension TestViewController : UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+}
+
